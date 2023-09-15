3e2b7011d544b05187284168b74b5f7af102eded
승연 정 <wjdtmd486@naver.com>
2023-09-15 15:32:08 +0900

    InitDevice 까지 구현

--- DirectX11_Homework_JSY/Drawing a Triangle/Drawing a Triangle.cpp
+++ DirectX11_Homework_JSY/Drawing a Triangle/Drawing a Triangle.cpp
@@ -1,180 +1,340 @@
-﻿// Drawing a Triangle.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
+﻿#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define UNICODE
+#include <windows.h>
+#include <d3d11_1.h>
+#pragma comment(lib, "d3d11.lib")
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
 
-#include "framework.h"
-#include "Drawing a Triangle.h"
+#include <assert.h>
 
-#define MAX_LOADSTRING 100
+//--------------------------------------------------------------------------------------
+// Global Variables
+//--------------------------------------------------------------------------------------
+static bool             global_windowDidResize = false;
+HWND                    g_hwnd;
+ID3D11Device1*          g_d3d11Device;
+ID3D11DeviceContext1*   g_d3d11DeviceContext;
+IDXGISwapChain1*        g_d3d11SwapChain;
+ID3D11RenderTargetView* g_d3d11FrameBufferView;
+ID3D11VertexShader*     g_vertexShader;
+ID3D11PixelShader*      g_pixelShader;
+ID3D11InputLayout*      g_inputLayout;
+ID3D11Buffer*           g_vertexBuffer;
+UINT                    g_numVerts;
+UINT                    g_stride;
+UINT                    g_offset;
 
-// 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+//--------------------------------------------------------------------------------------
+// Forward declarations
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+HRESULT InitWindow(HINSTANCE hInstance);
+HRESULT InitDevice();
+void CleanupDevice();
+void Render();
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: 여기에 코드를 입력합니다.
-
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_DRAWINGATRIANGLE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
-
-    // 애플리케이션 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (FAILED(InitWindow(hInstance)))
     {
-        return FALSE;
+        return GetLastError();
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DRAWINGATRIANGLE));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    if (FAILED(InitDevice()))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        CleanupDevice();
+        return GetLastError();
+    }
+  
+    // Main message loop
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
+        else
+        {
+            Render();
+        }
     }
 
-    return (int) msg.wParam;
+    CleanupDevice();
+
+    return (int)msg.wParam;
 }
 
-
-
-//
-//  함수: MyRegisterClass()
-//
-//  용도: 창 클래스를 등록합니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
+// 윈도우 프로시저 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DRAWINGATRIANGLE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DRAWINGATRIANGLE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
-//
-//   주석:
-//
-//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
-//        주 프로그램 창을 만든 다음 표시합니다.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  용도: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
+    LRESULT result = 0;
+    switch (msg)
     {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
+    case WM_KEYDOWN:
+    {
+        if (wparam == VK_ESCAPE)
+            DestroyWindow(hwnd);
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
+    }
     case WM_DESTROY:
+    {
         PostQuitMessage(0);
         break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
     }
-    return 0;
-}
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
+    case WM_SIZE:
     {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
+        // 윈도우 사이즈 가 변경되었을경우 true로 변경
+        global_windowDidResize = true;
         break;
     }
-    return (INT_PTR)FALSE;
+    default:
+        result = DefWindowProcW(hwnd, msg, wparam, lparam);
+    }
+    return result;
+}
+
+HRESULT InitWindow(HINSTANCE hInstance)
+{
+    // Open a window    
+    WNDCLASSEXW winClass = {};
+    winClass.cbSize = sizeof(WNDCLASSEXW);
+    winClass.style = CS_HREDRAW | CS_VREDRAW;
+    winClass.lpfnWndProc = &WndProc;
+    winClass.hInstance = hInstance;
+    winClass.hIcon = LoadIconW(0, IDI_APPLICATION);
+    winClass.hCursor = LoadCursorW(0, IDC_ARROW);
+    winClass.lpszClassName = L"MyWindowClass";
+    winClass.hIconSm = LoadIconW(0, IDI_APPLICATION);
+
+    if (!RegisterClassExW(&winClass)) {
+        MessageBoxA(0, "RegisterClassEx failed", "Fatal Error", MB_OK);
+        return E_FAIL;
+    }
+
+    RECT initialRect = { 0, 0, 1024, 768 };
+    AdjustWindowRectEx(&initialRect, WS_OVERLAPPEDWINDOW, FALSE, WS_EX_OVERLAPPEDWINDOW);
+    LONG initialWidth = initialRect.right - initialRect.left;
+    LONG initialHeight = initialRect.bottom - initialRect.top;
+
+    g_hwnd = CreateWindowExW(WS_EX_OVERLAPPEDWINDOW,
+        winClass.lpszClassName,
+        L"Drawing a Triangle",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        initialWidth,
+        initialHeight,
+        0, 0, hInstance, 0);
+
+    if (!g_hwnd)
+    {
+        MessageBoxA(0, "CreateWindowEx failed", "Fatal Error", MB_OK);
+        return E_FAIL;
+    }    
+}
+
+HRESULT InitDevice()
+{
+    // Create D3D11 Device and Context
+    {
+        ID3D11Device* baseDevice;
+        ID3D11DeviceContext* baseDeviceContext;
+        D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0 };
+        UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
+        #if defined( _DEBUG )
+                creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+        #endif
+
+        HRESULT hResult = D3D11CreateDevice(0, D3D_DRIVER_TYPE_HARDWARE,
+            0, creationFlags, featureLevels, ARRAYSIZE(featureLevels),
+            D3D11_SDK_VERSION, &baseDevice,
+            0, &baseDeviceContext);
+
+        if (FAILED(hResult)) {
+            MessageBoxA(0, "D3D11CreateDevice() failed", "Fatal Error", MB_OK);
+            return E_FAIL;
+        }
+
+        // Get 1.1 Interface of D3D11 Device and Context
+        hResult = baseDevice->QueryInterface(__uuidof(ID3D11Device1), (void**)&g_d3d11Device);
+        assert(SUCCEEDED(hResult));
+        baseDevice->Release();
+
+        hResult = baseDeviceContext->QueryInterface(__uuidof(ID3D11DeviceContext1), (void**)&g_d3d11DeviceContext);
+        assert(SUCCEEDED(hResult));
+        baseDeviceContext->Release();
+    }
+
+    #if defined( _DEBUG )
+        // Set up debug layer to break on D3D11 errors
+        ID3D11Debug* d3dDebug = nullptr;
+        g_d3d11Device->QueryInterface(__uuidof(ID3D11Debug), (void**)&d3dDebug);
+        if (d3dDebug)
+        {
+            ID3D11InfoQueue* d3dInfoQueue = nullptr;
+            if (SUCCEEDED(d3dDebug->QueryInterface(__uuidof(ID3D11InfoQueue), (void**)&d3dInfoQueue)))
+            {
+                d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
+                d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true);
+                d3dInfoQueue->Release();
+            }
+            d3dDebug->Release();
+        }
+    #endif
+
+    // Create Swap Chain
+    {
+        // Get DXGI Factory (needed to create Swap Chain)
+        IDXGIFactory2* dxgiFactory;
+        {
+            IDXGIDevice1* dxgiDevice;
+            HRESULT hResult = g_d3d11Device->QueryInterface(__uuidof(IDXGIDevice1), (void**)&dxgiDevice);
+            assert(SUCCEEDED(hResult));
+
+            IDXGIAdapter* dxgiAdapter;
+            hResult = dxgiDevice->GetAdapter(&dxgiAdapter);
+            assert(SUCCEEDED(hResult));
+            dxgiDevice->Release();
+
+            DXGI_ADAPTER_DESC adapterDesc;
+            dxgiAdapter->GetDesc(&adapterDesc);
+
+            OutputDebugStringA("Graphics Device: ");
+            OutputDebugStringW(adapterDesc.Description);
+
+            hResult = dxgiAdapter->GetParent(__uuidof(IDXGIFactory2), (void**)&dxgiFactory);
+            assert(SUCCEEDED(hResult));
+
+            dxgiAdapter->Release();
+        }
+
+        // SwapChain description 클래스
+        DXGI_SWAP_CHAIN_DESC1 d3d11SwapChainDesc = {};
+        d3d11SwapChainDesc.Width = 0; // use window width
+        d3d11SwapChainDesc.Height = 0; // use window height
+        d3d11SwapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM_SRGB;
+        d3d11SwapChainDesc.SampleDesc.Count = 1;
+        d3d11SwapChainDesc.SampleDesc.Quality = 0;
+        d3d11SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        d3d11SwapChainDesc.BufferCount = 2;
+        d3d11SwapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+        d3d11SwapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+        d3d11SwapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+        d3d11SwapChainDesc.Flags = 0;
+
+        // SwapChain 생성
+        HRESULT hResult = dxgiFactory->CreateSwapChainForHwnd(g_d3d11Device, g_hwnd, &d3d11SwapChainDesc, 0, 0, &g_d3d11SwapChain);
+        assert(SUCCEEDED(hResult));
+
+        dxgiFactory->Release();
+    }
+
+    // Create Framebuffer Render Target
+    {
+        ID3D11Texture2D* d3d11FrameBuffer;
+        HRESULT hResult = g_d3d11SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&d3d11FrameBuffer);
+        assert(SUCCEEDED(hResult));
+
+        hResult = g_d3d11Device->CreateRenderTargetView(d3d11FrameBuffer, 0, &g_d3d11FrameBufferView);
+        assert(SUCCEEDED(hResult));
+        d3d11FrameBuffer->Release();
+    }
+
+    // Create Vertex Shader
+    ID3DBlob* vsBlob;
+    {
+        ID3DBlob* shaderCompileErrorsBlob;
+        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "VS_main", "vs_5_0", 0, 0, &vsBlob, &shaderCompileErrorsBlob);
+        if (FAILED(hResult))
+        {
+            const char* errorString = NULL;
+            if (hResult == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+                errorString = "Could not compile shader; file not found";
+            else if (shaderCompileErrorsBlob) {
+                errorString = (const char*)shaderCompileErrorsBlob->GetBufferPointer();
+                shaderCompileErrorsBlob->Release();
+            }
+            MessageBoxA(0, errorString, "Shader Compiler Error", MB_ICONERROR | MB_OK);
+            return E_FAIL;
+        }
+
+        hResult = g_d3d11Device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &g_vertexShader);
+        assert(SUCCEEDED(hResult));
+    }
+
+    // Create Pixel Shader
+    {
+        ID3DBlob* psBlob;
+        ID3DBlob* shaderCompileErrorsBlob;
+        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "PS_main", "ps_5_0", 0, 0, &psBlob, &shaderCompileErrorsBlob);
+        if (FAILED(hResult))
+        {
+            const char* errorString = NULL;
+            if (hResult == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))
+                errorString = "Could not compile shader; file not found";
+            else if (shaderCompileErrorsBlob) {
+                errorString = (const char*)shaderCompileErrorsBlob->GetBufferPointer();
+                shaderCompileErrorsBlob->Release();
+            }
+            MessageBoxA(0, errorString, "Shader Compiler Error", MB_ICONERROR | MB_OK);
+            return E_FAIL;
+        }
+
+        hResult = g_d3d11Device->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &g_pixelShader);
+        assert(SUCCEEDED(hResult));
+        psBlob->Release();
+    }
+
+    // Create Input Layout
+    {
+        D3D11_INPUT_ELEMENT_DESC inputElementDesc[] =
+        {
+            { "POS", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "COL", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+        };
+
+        HRESULT hResult = g_d3d11Device->CreateInputLayout(inputElementDesc, ARRAYSIZE(inputElementDesc), vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &g_inputLayout);
+        assert(SUCCEEDED(hResult));
+        vsBlob->Release();
+    }
+
+    // Create Vertex Buffer
+    {
+        float vertexData[] = { // x, y, r, g, b, a
+            0.0f - 0.5f,  0.5f, 0.f, 1.f, 0.f, 1.f,
+            0.5f - 0.5f, -0.5f, 1.f, 0.f, 0.f, 1.f,
+            -0.5f - 0.5f, -0.5f, 0.f, 0.f, 1.f, 1.f
+        };
+        g_stride = 6 * sizeof(float);
+        g_numVerts = sizeof(vertexData) / g_stride;
+        g_offset = 0.f;
+
+        D3D11_BUFFER_DESC vertexBufferDesc = {};
+        vertexBufferDesc.ByteWidth = sizeof(vertexData);
+        vertexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+        vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+        D3D11_SUBRESOURCE_DATA vertexSubresourceData = { vertexData };
+
+        HRESULT hResult = g_d3d11Device->CreateBuffer(&vertexBufferDesc, &vertexSubresourceData, &g_vertexBuffer);
+        assert(SUCCEEDED(hResult));
+    }
+}
+
+//--------------------------------------------------------------------------------------
+// Clean up the objects we've created
+//--------------------------------------------------------------------------------------
+void CleanupDevice()
+{
+
+}
+
+//--------------------------------------------------------------------------------------
+// Render a frame
+//--------------------------------------------------------------------------------------
+void Render()
+{
 }
